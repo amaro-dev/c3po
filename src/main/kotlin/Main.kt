6e59d0c4a202490e2e12314d8665abe016a70c5c
Rodrigo Amaro <rodrigo.arodrigues@mercadolivre.com>
2024-04-17 15:48:08 -0300

    Search packages feature
    ### Added
    - A field on the PackageTab so the user can search

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,8 +8,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -24,6 +25,7 @@ import models.AdbDevice
 import models.AppPackage
 import models.DeviceAttrs
 import ui.PackageRow
+import ui.useDebounce
 import java.io.InputStreamReader
 
 @Composable
@@ -93,18 +95,38 @@ fun DeviceDetailsTab(currentDevice: AdbDevice?) {
 
 @Composable
 fun PackagesTab(currentDevice: AdbDevice?, packageList: List<AppPackage>) {
+    var searchTerm by remember { mutableStateOf("") }
+    var filter by remember { mutableStateOf("") }
+    searchTerm.useDebounce {
+        filter = it
+    }
     Box(Modifier.fillMaxSize().padding(10.dp)) {
+        Column {
+            OutlinedTextField(
+                searchTerm,
+                onValueChange = { searchTerm = it },
+                leadingIcon = { Icon(Icons.Filled.Search, "") },
+                placeholder = { Text("Type to search") },
+                modifier = Modifier.fillMaxWidth()
+            )
 
-        val listState = rememberLazyListState()
-        LazyColumn(Modifier.fillMaxSize().padding(end = 12.dp), state = listState) {
-            items(packageList) { pkg ->
-                PackageRow(pkg, currentDevice) { cmd -> cmd.run() }
+            Spacer(Modifier.height(8.dp))
+            Box {
+                val listState = rememberLazyListState()
+                LazyColumn(Modifier.fillMaxSize().padding(end = 12.dp), state = listState) {
+                    items(packageList.filter {
+                        filter.length < 3 || it.packageName.contains((filter))
+                    }) { pkg ->
+                        PackageRow(pkg, currentDevice) { cmd -> cmd.run() }
+                    }
+                }
+                VerticalScrollbar(
+                    modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+                    adapter = rememberScrollbarAdapter(scrollState = listState)
+                )
             }
         }
-        VerticalScrollbar(
-            modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
-            adapter = rememberScrollbarAdapter(scrollState = listState)
-        )
+
     }
 }
 
