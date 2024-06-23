387c4e4778a69c43a0908f77c58fe66ecafd8ce5
Rodrigo Amaro <rodrigo.arodrigues@mercadolivre.com>
2024-06-23 10:53:32 -0300

    Copy Icon style fix

--- src/main/kotlin/ui/plugins/attrs/DeviceAttrRow.kt
+++ src/main/kotlin/ui/plugins/attrs/DeviceAttrRow.kt
@@ -1,19 +1,21 @@
 package ui.plugins.attrs
 
 import androidx.compose.animation.*
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -51,22 +53,33 @@ fun DeviceAttrRow(label: String, value: String?, onAction: (IAction) -> Unit) {
                     enter = slideInHorizontallyFromRight(),
                     exit = slideOutHorizontallyToRight()
                 ) {
-                    Box(Modifier.padding(end = 10.dp).onHover { isHoveringValue = it }) {
+                    Box(
+                        Modifier.padding(end = 10.dp)
+                            .onHover { isHoveringValue = it }
+                            .clip(RoundedCornerShape(16.dp))
+                    ) {
                         Icon(
-                            Icons.Default.Info,
+                            painterResource("ic_copy.svg"),
                             contentDescription = "",
                             modifier = Modifier.clickable { onAction(Action.CopyText(value ?: "")) }
+                                .withIconStyle()
                         )
                     }
                 }
             }
         }
         AnimatedVisibility(isHoveringAttr, enter = slideInHorizontally(), exit = slideOutHorizontally()) {
-            Box(Modifier.padding(start = 10.dp).onHover { isHoveringAttr = it }) {
+            Box(
+                modifier = Modifier.padding(start = 10.dp)
+                    .onHover { isHoveringAttr = it }
+                    .clip(RoundedCornerShape(16.dp))
+            ) {
                 Icon(
-                    Icons.Default.Info,
+                    painterResource("ic_copy.svg"),
                     contentDescription = "",
                     modifier = Modifier.clickable { onAction(Action.CopyText(label.removeSuffix(":"))) }
+                        .withIconStyle()
+
                 )
             }
         }
@@ -85,3 +98,11 @@ fun Modifier.onHover(event: (Boolean) -> Unit) = this.onPointerEvent(
 
 fun slideInHorizontallyFromRight() = slideIn(initialOffset = { IntOffset(it.width, 0) })
 fun slideOutHorizontallyToRight() = slideOut(targetOffset = { IntOffset(it.width, 0) })
+
+@Composable
+fun Modifier.withIconStyle(): Modifier = this.background(
+    color = MaterialTheme.colors.background,
+    shape = RoundedCornerShape(16.dp)
+)
+    .padding(6.dp)
+    .clip(shape = RoundedCornerShape(16.dp))
