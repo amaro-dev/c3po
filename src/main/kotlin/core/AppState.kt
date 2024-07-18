03decc4d6cbdc486b0ff415dd0ab706abb2ad193
Rodrigo Amaro <rodrigo.arodrigues@mercadolivre.com>
2024-07-18 10:37:53 -0300

    Remove unused properties

--- src/main/kotlin/core/AppState.kt
+++ src/main/kotlin/core/AppState.kt
@@ -1,16 +1,12 @@
 package core
 
-import models.ActivityInfo
 import models.AdbDevice
-import models.AppPackage
 import java.util.*
 
-data class AppState (
+data class AppState(
     val devices: List<AdbDevice> = emptyList(),
     val currentDevice: AdbDevice? = null,
-    val packages: List<AppPackage> = emptyList(),
     val currentPlugin: String? = null,
-    val activities: List<ActivityInfo> = emptyList(),
     val windows: Map<String, Any> = emptyMap(),
     val settings: Properties = Properties(),
     val settingsState: SettingsState = SettingsState.NotInitialized,
