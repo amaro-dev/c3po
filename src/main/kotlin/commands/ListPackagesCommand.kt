33a7e69fe886e9358a3d6ea96221c8d830bc3ac4
Rodrigo Amaro <rodrigo.arodrigues@mercadolivre.com>
2024-08-01 13:41:02 -0300

    Fixes the new ListPackagesCommand for Newland devices

--- src/main/kotlin/commands/ListPackagesCommand.kt
+++ src/main/kotlin/commands/ListPackagesCommand.kt
@@ -9,18 +9,18 @@ class ListPackagesCommand(device: AdbDevice): AdbCommand<List<AppPackage>> {
 
     override fun parse(result: CommandResult): List<AppPackage> {
         val packageLinePart = "\\s{2}Package\\s\\[(.*)\\].*"
-        val ignoredLinesPart = "(\\r\\n\\s{4}.*)*"
-        val versionCodeAndTargetPart = "\\r\\n\\s{4}versionCode=(\\d+)\\stargetSdk=(\\d+)"
-        val versionNamePart = "\\r\\n\\s{4}versionName=(.*)"
-        val regex = Regex("$packageLinePart$ignoredLinesPart$versionCodeAndTargetPart$versionNamePart")
+        val ignoredLinesPart = "(?:\\r?\\n\\s{4}.*)*"
+        val versionCodeAndTargetPart = "\\r?\\n\\s{4}versionCode=(\\d+).*\\stargetSdk=(\\d+)"
+        val versionNamePart = "\\r?\\n\\s{4}versionName=(.*)"
+        val regex = Regex("$packageLinePart$ignoredLinesPart$versionCodeAndTargetPart$ignoredLinesPart$versionNamePart")
         return result.content.substring(result.content.indexOf("Packages:"))
             .let {
                 regex.findAll(it).map {
                     AppPackage(
                         it.groups[1]?.value ?: "",
-                        try { it.groups[5]?.value ?: "" } catch (ex: IndexOutOfBoundsException) { "" },
-                        try { it.groups[3]?.value?.toInt() ?: -1 } catch (ex: IndexOutOfBoundsException) { -1 },
-                        try { it.groups[4]?.value?.toInt() ?: -1 } catch (ex: IndexOutOfBoundsException) { -1 }
+                        try { it.groups[4]?.value ?: "" } catch (ex: IndexOutOfBoundsException) { "" },
+                        try { it.groups[2]?.value?.toInt() ?: -1 } catch (ex: IndexOutOfBoundsException) { -1 },
+                        try { it.groups[3]?.value?.toInt() ?: -1 } catch (ex: IndexOutOfBoundsException) { -1 }
                     )
                 }
             }
