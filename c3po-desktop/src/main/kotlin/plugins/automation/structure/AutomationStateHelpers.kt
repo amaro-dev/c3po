583f1d364bced50d5516587178e7059f0f0c26b6
Amaro, Rodrigo <rodrigo@amaro.dev>
2025-09-02 08:31:08 -0300

    Fixes: - Saving the script won't close it

--- c3po-desktop/src/main/kotlin/plugins/automation/structure/AutomationStateHelpers.kt
+++ c3po-desktop/src/main/kotlin/plugins/automation/structure/AutomationStateHelpers.kt
@@ -8,16 +8,3 @@ fun IProcessor<AppState>.deliver(pluginName: String, state: AutomationState) {
     reduce(Action.DeliverPluginResult(pluginName, listOf(state)))
 }
 
-fun updateScriptStep(script: Script, stepIndex: Int, newStep: ScriptStep): Script {
-    val updatedSteps = script.steps.toMutableList()
-    updatedSteps[stepIndex] = newStep
-    return script.copy(steps = updatedSteps)
-}
-
-fun closeAllDialogs(state: AutomationState): AutomationState =
-    state.copy(
-        editingStepIndex = null,
-        showPackageSelector = false,
-        showActivitySelector = false,
-        showApkPicker = false
-    )
