d0c637324e0baf3cfb6e1cb6d40aeb12ce6fefea
Amaro, Rodrigo <rodrigo@amaro.dev>
2025-09-17 09:09:58 -0300

    Fixing the tests for SettingsMiddlewareTest.kt

--- c3po-core/src/test/kotlin/core/model/SettingsMiddlewareTest.kt
+++ c3po-core/src/test/kotlin/core/model/SettingsMiddlewareTest.kt
@@ -12,6 +12,8 @@ import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 import java.util.Properties
 
@@ -19,33 +21,34 @@ class SettingsMiddlewareTest {
     val commandExecutor: SystemCommandExecutor = mockk(relaxed = true)
 
     @Test
-    fun `Handle LoadSettings action`() {
+    fun `Handle LoadSettings action`() = runTest {
         val properties: Properties = mockk(relaxed = true)
         val settingsRepository: SettingsRepository =
             mockk(relaxed = true) {
                 every { load() } returns Result.success(properties)
             }
-        val middleware = SettingsMiddleware(settingsRepository, commandExecutor)
+        val middleware = SettingsMiddleware(settingsRepository, commandExecutor, this)
         val processor: IProcessor<AppState> = mockk(relaxed = true)
 
         middleware.process(Action.LoadSettings, mockk(), processor)
-
+        advanceUntilIdle()
         verify {
             processor.reduce(Action.LoadSettingsIntoState(properties))
         }
     }
 
     @Test
-    fun `Handle LoadSettings action triggers RefreshDevices`() {
+    fun `Handle LoadSettings action triggers RefreshDevices`() = runTest {
         val properties: Properties = mockk(relaxed = true)
         val settingsRepository: SettingsRepository =
             mockk(relaxed = true) {
                 every { load() } returns Result.success(properties)
             }
-        val middleware = SettingsMiddleware(settingsRepository, commandExecutor)
+        val middleware = SettingsMiddleware(settingsRepository, commandExecutor, this)
         val processor: IProcessor<AppState> = mockk(relaxed = true)
 
         middleware.process(Action.LoadSettings, mockk(), processor)
+        advanceUntilIdle()
 
         verify {
             processor.perform(Action.RefreshDevices)
@@ -53,15 +56,16 @@ class SettingsMiddlewareTest {
     }
 
     @Test
-    fun `Handle LoadSettings informs when failing to load settings`() {
+    fun `Handle LoadSettings informs when failing to load settings`() = runTest {
         val settingsRepository: SettingsRepository =
             mockk(relaxed = true) {
                 every { load() } returns Result.failure(Exception())
             }
-        val middleware = SettingsMiddleware(settingsRepository, commandExecutor)
+        val middleware = SettingsMiddleware(settingsRepository, commandExecutor, this)
         val processor: IProcessor<AppState> = mockk(relaxed = true)
 
         middleware.process(Action.LoadSettings, mockk(), processor)
+        advanceUntilIdle()
 
         verify {
             processor.reduce(Action.SettingsNotFound)
@@ -69,17 +73,19 @@ class SettingsMiddlewareTest {
     }
 
     @Test
-    fun `Handle ChangeSettingsProperty action`() {
+    fun `Handle ChangeSettingsProperty action`() = runTest {
         val properties = Properties()
         properties["prop"] = "value"
         val state = AppState(settings = properties)
         val settingsRepository: SettingsRepository = mockk(relaxed = true)
-        val middleware = SettingsMiddleware(settingsRepository, commandExecutor)
+        val middleware = SettingsMiddleware(settingsRepository, commandExecutor, this)
         val processor: IProcessor<AppState> = mockk(relaxed = true)
 
         middleware.process(Action.ChangeSettingsProperty("prop", "new-value"), state, processor)
 
         val slot = CapturingSlot<Action.LoadSettingsIntoState>()
+        advanceUntilIdle()
+
         verify {
             processor.reduce(capture(slot))
         }
@@ -87,16 +93,17 @@ class SettingsMiddlewareTest {
     }
 
     @Test
-    fun `If ADB path setting was changed, refresh available devices`() {
+    fun `If ADB path setting was changed, refresh available devices`() = runTest {
         val properties = Properties()
         properties[Settings.ADB_PATH_PROP] = "value"
         val state = AppState(settings = properties)
         val settingsRepository: SettingsRepository = mockk(relaxed = true)
         coEvery { commandExecutor.executeCommand(any(), any()) } returns Result.success("")
-        val middleware = SettingsMiddleware(settingsRepository, commandExecutor)
+        val middleware = SettingsMiddleware(settingsRepository, commandExecutor, this)
         val processor: IProcessor<AppState> = mockk(relaxed = true)
 
         middleware.process(Action.ChangeSettingsProperty(Settings.ADB_PATH_PROP, "new-value"), state, processor)
+        advanceUntilIdle()
 
         verify {
             processor.perform(Action.RefreshDevices)
@@ -104,14 +111,15 @@ class SettingsMiddlewareTest {
     }
 
     @Test
-    fun `Handle SaveSettings action`() {
+    fun `Handle SaveSettings action`() = runTest {
         val settings: Properties = mockk(relaxed = true)
         val state = AppState(settings = settings)
         val settingsRepository: SettingsRepository = mockk(relaxed = true)
-        val middleware = SettingsMiddleware(settingsRepository, commandExecutor)
+        val middleware = SettingsMiddleware(settingsRepository, commandExecutor, this)
         val processor: IProcessor<AppState> = mockk(relaxed = true)
 
         middleware.process(Action.SaveSettings, state, processor)
+        advanceUntilIdle()
 
         verify {
             settingsRepository.save(settings)
@@ -119,16 +127,18 @@ class SettingsMiddlewareTest {
     }
 
     @Test
-    fun `Handle dark mode setting change`() {
+    fun `Handle dark mode setting change`() = runTest {
         val properties = Properties()
         val state = AppState(settings = properties)
         val settingsRepository: SettingsRepository = mockk(relaxed = true)
-        val middleware = SettingsMiddleware(settingsRepository, commandExecutor)
+        val middleware = SettingsMiddleware(settingsRepository, commandExecutor, this)
         val processor: IProcessor<AppState> = mockk(relaxed = true)
 
         middleware.process(Action.ChangeSettingsProperty(Settings.DARK_MODE_PROP, "true"), state, processor)
 
         val slot = CapturingSlot<Action.LoadSettingsIntoState>()
+        advanceUntilIdle()
+
         verify {
             processor.reduce(capture(slot))
         }
